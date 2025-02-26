cffac3f70ab2a197b9146149b1a16443f8721ca3
jreyesr <jreyesr@users.noreply.github.com>
2025-02-25 19:11:27 -0500

    Initial commit, base Render operation

--- README.md
+++ README.md
@@ -6,28 +6,11 @@ This repo contains example nodes to help you get started building your own custo
 
 To make your custom node available to the community, you must create it as an npm package, and [submit it to the npm registry](https://docs.npmjs.com/packages-and-modules/contributing-packages-to-the-registry).
 
-## Prerequisites
-
-You need the following installed on your development machine:
-
-* [git](https://git-scm.com/downloads)
-* Node.js and pnpm. Minimum version Node 18. You can find instructions on how to install both using nvm (Node Version Manager) for Linux, Mac, and WSL [here](https://github.com/nvm-sh/nvm). For Windows users, refer to Microsoft's guide to [Install NodeJS on Windows](https://docs.microsoft.com/en-us/windows/dev-environment/javascript/nodejs-on-windows).
-* Install n8n with:
-  ```
-  pnpm install n8n -g
-  ```
-* Recommended: follow n8n's guide to [set up your development environment](https://docs.n8n.io/integrations/creating-nodes/build/node-development-environment/).
 
 ## Using this starter
 
 These are the basic steps for working with the starter. For detailed guidance on creating and publishing nodes, refer to the [documentation](https://docs.n8n.io/integrations/creating-nodes/).
 
-1. [Generate a new repository](https://github.com/n8n-io/n8n-nodes-starter/generate) from this template repository.
-2. Clone your new repo:
-   ```
-   git clone https://github.com/<your organization>/<your-repo-name>.git
-   ```
-3. Run `pnpm i` to install dependencies.
 4. Open the project in your editor.
 5. Browse the examples in `/nodes` and `/credentials`. Modify the examples, or replace them with your own nodes.
 6. Update the `package.json` to match your details.
---
+++ nodes/DocxTemplater/DocxTemplater.node.ts
@@ -0,0 +1,201 @@
+import {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	JsonObject,
+	NodeApiError,
+	NodeOperationError,
+} from 'n8n-workflow';
+import PizZip from 'pizzip';
+import Docxtemplater from 'docxtemplater';
+
+export class DocxTemplater implements INodeType {
+	description: INodeTypeDescription = {
+		properties: [
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				// displayOptions: {
+				// 	show: {
+				// 		resource: ['contact'],
+				// 	},
+				// },
+				options: [
+					{
+						name: 'Render',
+						value: 'render',
+						description: 'Render a template into a document',
+						action: 'Render a template',
+					},
+				],
+				default: 'render',
+				noDataExpression: true,
+			},
+			{
+				displayName: 'Input file name',
+				name: 'binaryProperty',
+				type: 'string',
+				displayOptions: {
+					show: {
+						operation: ['render'],
+					},
+				},
+				default: 'data',
+				noDataExpression: true,
+			},
+			{
+				displayName: 'Data',
+				name: 'context',
+				type: 'json',
+				displayOptions: {
+					show: {
+						operation: ['render'],
+					},
+				},
+				default: '{}',
+			},
+			{
+				displayName: 'Options',
+				name: 'options',
+				type: 'collection',
+				placeholder: 'Add option',
+				default: {},
+				options: [
+					{
+						displayName: 'Paragraph Loop',
+						name: 'paragraphLoop',
+						type: 'boolean',
+						default: true,
+						description:
+							"If enabled, looped paragraphs won't have additional spaces due to the loop tags. See <a href='https://docxtemplater.com/docs/configuration/#how-to-use-it'>https://docxtemplater.com/docs/configuration/#how-to-use-it</a>.",
+					},
+					{
+						displayName: 'Render Newlines',
+						name: 'linebreaks',
+						type: 'boolean',
+						default: true,
+						description:
+							"If enabled, newline characters (\\n) in tags will show up in the output, otherwise they will be ignored. See <a href='https://docxtemplater.com/docs/configuration/#linebreaks'>https://docxtemplater.com/docs/configuration/#linebreaks</a>.",
+					},
+					{
+						displayName: 'Enable Angular parser',
+						name: 'enableAngularParser',
+						type: 'boolean',
+						default: true,
+						description:
+							"If enabled, Angular expressions (e.g. addition, nested property access and filters) will be available in the template. See <a href='https://docxtemplater.com/docs/angular-parse/'>https://docxtemplater.com/docs/angular-parse/</a>.",
+					},
+					{
+						displayName: 'Put Output File in Field',
+						name: 'binaryPropertyOutput',
+						type: 'string',
+						default: 'data',
+						hint: 'The name of the output binary field to put the rendered document in',
+					},
+					{
+						displayName: 'File Name',
+						name: 'outputFileName',
+						type: 'string',
+						default: 'rendered.docx',
+						description: 'Name of the output file',
+					},
+				],
+				displayOptions: {
+					show: {
+						operation: ['render'],
+					},
+				},
+			},
+		],
+		displayName: 'DocxTemplater',
+		name: 'docxTemplater',
+		// icon: 'file:friendGrid.svg',
+		group: ['transform'],
+		version: 1,
+		description: 'Generate an Office document from a template',
+		defaults: {
+			name: 'DocxTemplater',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+		let responseData;
+		const returnData: INodeExecutionData[] = [];
+		const operation = this.getNodeParameter('operation', 0);
+
+		for (let i = 0; i < items.length; i++) {
+			try {
+				if (operation === 'render') {
+					const inputFileProperty = this.getNodeParameter('binaryProperty', i);
+					const outputFileProperty = this.getNodeParameter(
+						'options.binaryPropertyOutput',
+						i,
+						'data',
+						{
+							ensureType: 'string',
+						},
+					) as string;
+					const outputFileName = this.getNodeParameter(
+						'options.outputFileName',
+						i,
+						'rendered.docx',
+						{
+							ensureType: 'string',
+						},
+					) as string;
+					const context = this.getNodeParameter('context', i, {}, { ensureType: 'json' });
+
+					const inputDataBuffer = await this.helpers.getBinaryDataBuffer(i, inputFileProperty);
+					const zip = new PizZip(inputDataBuffer);
+					const doc = new Docxtemplater(zip, {
+						paragraphLoop: this.getNodeParameter('options.paragraphLoop', i, true, {
+							extractValue: true,
+							ensureType: 'boolean',
+						}) as boolean,
+						linebreaks: true,
+					});
+					this.logger.debug('render', {
+						inputFileName: inputFileProperty,
+						inputDataBuffer: inputDataBuffer.length,
+						context,
+					});
+					doc.render(context);
+					const outputDataBuffer = doc
+						.getZip()
+						.generate({ type: 'nodebuffer', compression: 'DEFLATE' });
+
+					const binaryData = await this.helpers.prepareBinaryData(outputDataBuffer, outputFileName);
+					responseData = {
+						binary: {
+							[outputFileProperty]: binaryData,
+						},
+						json: {},
+						pairedItem: { item: i },
+					};
+					returnData.push(responseData);
+				}
+			} catch (error) {
+				const nodeOperationError = new NodeOperationError(this.getNode(), error, {
+					itemIndex: i,
+					message: 'Error while rendering',
+				});
+				if (this.continueOnFail()) {
+					returnData.push({
+						json: {
+							error: nodeOperationError.message,
+						},
+						pairedItem: { item: i },
+					});
+					continue;
+				}
+				throw new NodeApiError(this.getNode(), error as JsonObject, { itemIndex: i });
+			}
+		}
+		return [returnData];
+	}
+}
